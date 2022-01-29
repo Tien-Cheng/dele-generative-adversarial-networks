a4a4ef02413081b684d14962bca79ecf93d739eb
Tien-Cheng <tiencheng@protonmail.com>
2022-01-29 22:48:04 +0800

    [feat] Move unnormalize to Layers

--- utils/layers.py
+++ utils/layers.py
@@ -1,5 +1,22 @@
 import torch
+import torchvision.transforms as T
 from torch import nn
+
+
+class NormalizeInverse(T.Normalize):
+    """
+    Undoes the normalization and returns the reconstructed images in the input domain.
+    """
+
+    def __init__(self, mean, std):
+        mean = torch.as_tensor(mean)
+        std = torch.as_tensor(std)
+        std_inv = 1 / (std + 1e-7)
+        mean_inv = -mean * std_inv
+        super().__init__(mean=mean_inv, std=std_inv)
+
+    def __call__(self, tensor):
+        return super().__call__(tensor.clone())
 
 class ConditionalBatchNorm2d(nn.Module):
   def __init__(self, num_features, num_classes):
