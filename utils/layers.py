09c803f3670ca4919ccde45ce93359e8200aac2b
Tien-Cheng <tiencheng@protonmail.com>
2022-01-30 14:27:15 +0800

    [fix] Fix indentation

--- utils/layers.py
+++ utils/layers.py
@@ -20,33 +20,49 @@ class NormalizeInverse(T.Normalize):
     def __call__(self, tensor: torch.Tensor):
         return super().__call__(tensor.clone())
 
+
 class ConditionalBatchNorm2d(nn.Module):
-  def __init__(self, num_features: int, num_classes: int = 10):
-      """Conditional Batch Normalization from conditional Spectral Normalization GAN.
-      Works as a form of Batch Normalization, with the weights and biases dependent on the class. Initially introduced in cGANs with Projection Discriminator.
+    def __init__(self, num_features: int, num_classes: int = 10):
+        """Conditional Batch Normalization from conditional Spectral Normalization GAN.
+        Works as a form of Batch Normalization, with the weights and biases dependent on the class. Initially introduced in cGANs with Projection Discriminator.
 
-      Implementation taken from: https://github.com/pytorch/pytorch/issues/8985#issuecomment-405080775
+        Implementation taken from: https://github.com/pytorch/pytorch/issues/8985#issuecomment-405080775
 
-      :param num_features: [description]
-      :type num_features: [type]
-      :param num_classes: [description]
-      :type num_classes: [type]
-      """
-    super().__init__()
-    self.num_features = num_features
-    self.bn = nn.BatchNorm2d(num_features, affine=False)
-    self.embed = nn.Embedding(num_classes, num_features * 2)
-    self.embed.weight.data[:, :num_features].normal_(1, 0.02)  # Initialise scale at N(1, 0.02)
-    self.embed.weight.data[:, num_features:].zero_()  # Initialise bias at 0
+        :param num_features: [description]
+        :type num_features: [type]
+        :param num_classes: [description]
+        :type num_classes: [type]
+        """
+        super().__init__()
+        self.num_features = num_features
+        self.bn = nn.BatchNorm2d(num_features, affine=False)
+        self.embed = nn.Embedding(num_classes, num_features * 2)
+        self.embed.weight.data[:, :num_features].normal_(
+            1, 0.02
+        )  # Initialise scale at N(1, 0.02)
+        self.embed.weight.data[:, num_features:].zero_()  # Initialise bias at 0
 
-  def forward(self, x: torch.Tensor, y: torch.Tensor):
-    out = self.bn(x)
-    gamma, beta = self.embed(y).chunk(2, 1)
-    out = gamma.view(-1, self.num_features, 1, 1) * out + beta.view(-1, self.num_features, 1, 1)
-    return out
+    def forward(self, x: torch.Tensor, y: torch.Tensor):
+        out = self.bn(x)
+        gamma, beta = self.embed(y).chunk(2, 1)
+        out = gamma.view(-1, self.num_features, 1, 1) * out + beta.view(
+            -1, self.num_features, 1, 1
+        )
+        return out
+
 
 class ResidualBlockGenerator(nn.Module):
-    def __init__(self, in_ch: int, out_ch: int, kernel_size: int = 3, padding: int = 1, activation: callable = F.relu, upsample:bool =False, num_classes: int=10, use_spectral_norm: bool = True):
+    def __init__(
+        self,
+        in_ch: int,
+        out_ch: int,
+        kernel_size: int = 3,
+        padding: int = 1,
+        activation: callable = F.relu,
+        upsample: bool = False,
+        num_classes: int = 10,
+        use_spectral_norm: bool = True,
+    ):
         """Residual Block for the Generator, inspired by SNGANs. Implementation based on: https://github.com/ajbrock/BigGAN-PyTorch/blob/98459431a5d618d644d54cd1e9fceb1e5045648d/layers.py#L375
 
         :param in_ch: [description]
@@ -68,9 +84,9 @@ class ResidualBlockGenerator(nn.Module):
         """
         self.activation = activation
         self.num_classes = num_classes
-        self.upsample = None if upsample is False else nn.Upsample(scale_factor = 2)
-        self.conv1 = nn.Conv2d(in_ch, out_ch, kernel_size = kernel_size, padding = padding)
-        self.conv2 = nn.Conv2d(out_ch, out_ch, kernel_size = kernel_size, padding = padding)
+        self.upsample = None if upsample is False else nn.Upsample(scale_factor=2)
+        self.conv1 = nn.Conv2d(in_ch, out_ch, kernel_size=kernel_size, padding=padding)
+        self.conv2 = nn.Conv2d(out_ch, out_ch, kernel_size=kernel_size, padding=padding)
         if use_spectral_norm:
             self.conv1 = spectral_norm(self.conv1)
             self.conv2 = spectral_norm(self.conv2)
@@ -90,12 +106,20 @@ class ResidualBlockGenerator(nn.Module):
 
 
 class ResidualBlockDiscriminator(nn.Module):
-    def __init__(self, in_ch: int, out_ch: int, kernel_size: int = 3, padding: int = 1, activation: callable = F.relu, downsample: bool = False, use_spectral_norm: bool = True):
+    def __init__(
+        self,
+        in_ch: int,
+        out_ch: int,
+        kernel_size: int = 3,
+        padding: int = 1,
+        activation: callable = F.relu,
+        downsample: bool = False,
+        use_spectral_norm: bool = True,
+    ):
         self.activation = activation
         self.downsample = None if downsample else nn.AvgPool2d(2)
-        self.conv1 = nn.Conv2d(in_ch, out_ch, kernel_size = kernel_size, padding = padding
-        )
-        self.conv2 = nn.Conv2d(out_ch, out_ch, kernel_size = kernel_size, padding = padding)
+        self.conv1 = nn.Conv2d(in_ch, out_ch, kernel_size=kernel_size, padding=padding)
+        self.conv2 = nn.Conv2d(out_ch, out_ch, kernel_size=kernel_size, padding=padding)
 
         if use_spectral_norm:
             self.conv1 = spectral_norm(self.conv1)
@@ -109,4 +133,3 @@ class ResidualBlockDiscriminator(nn.Module):
         if self.downsample is not None:
             h = self.downsample(h)
         return h + self.downsample(x)
-        
