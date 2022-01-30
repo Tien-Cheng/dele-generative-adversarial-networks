f3278e62007550ddbe3b6788ced5d9617fa4b0c7
Tien-Cheng <tiencheng@protonmail.com>
2022-01-30 14:17:27 +0800

    [feat] Added Resblock for Discriminator

--- utils/layers.py
+++ utils/layers.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 import torchvision.transforms as T
 from torch import nn
 from torch.nn.utils import spectral_norm
@@ -22,7 +23,7 @@ class NormalizeInverse(T.Normalize):
 class ConditionalBatchNorm2d(nn.Module):
   def __init__(self, num_features: int, num_classes: int = 10):
       """Conditional Batch Normalization from conditional Spectral Normalization GAN.
-      Works as a form of Batch Normalization, with the weights and biases dependent on the class.
+      Works as a form of Batch Normalization, with the weights and biases dependent on the class. Initially introduced in cGANs with Projection Discriminator.
 
       Implementation taken from: https://github.com/pytorch/pytorch/issues/8985#issuecomment-405080775
 
@@ -46,6 +47,25 @@ class ConditionalBatchNorm2d(nn.Module):
 
 class ResidualBlockGenerator(nn.Module):
     def __init__(self, in_ch: int, out_ch: int, kernel_size: int = 3, padding: int = 1, activation: callable = F.relu, upsample:bool =False, num_classes: int=10, use_spectral_norm: bool = True):
+        """Residual Block for the Generator, inspired by SNGANs. Implementation based on: https://github.com/ajbrock/BigGAN-PyTorch/blob/98459431a5d618d644d54cd1e9fceb1e5045648d/layers.py#L375
+
+        :param in_ch: [description]
+        :type in_ch: int
+        :param out_ch: [description]
+        :type out_ch: int
+        :param kernel_size: [description], defaults to 3
+        :type kernel_size: int, optional
+        :param padding: [description], defaults to 1
+        :type padding: int, optional
+        :param activation: [description], defaults to F.relu
+        :type activation: callable, optional
+        :param upsample: [description], defaults to False
+        :type upsample: bool, optional
+        :param num_classes: [description], defaults to 10
+        :type num_classes: int, optional
+        :param use_spectral_norm: [description], defaults to True
+        :type use_spectral_norm: bool, optional
+        """
         self.activation = activation
         self.num_classes = num_classes
         self.upsample = None if upsample is False else nn.Upsample(scale_factor = 2)
@@ -69,3 +89,24 @@ class ResidualBlockGenerator(nn.Module):
         return h + self.upsample(x)
 
 
+class ResidualBlockDiscriminator(nn.Module):
+    def __init__(self, in_ch: int, out_ch: int, kernel_size: int = 3, padding: int = 1, activation: callable = F.relu, downsample: bool = False, use_spectral_norm: bool = True):
+        self.activation = activation
+        self.downsample = None if downsample else nn.AvgPool2d(2)
+        self.conv1 = nn.Conv2d(in_ch, out_ch, kernel_size = kernel_size, padding = padding
+        )
+        self.conv2 = nn.Conv2d(out_ch, out_ch, kernel_size = kernel_size, padding = padding)
+
+        if use_spectral_norm:
+            self.conv1 = spectral_norm(self.conv1)
+            self.conv2 = spectral_norm(self.conv2)
+
+    def forward(self, x):
+        h = self.activation(x)
+        h = self.conv1(h)
+        h = self.activation(h)
+        h = self.conv2(h)
+        if self.downsample is not None:
+            h = self.downsample(h)
+        return h + self.downsample(x)
+        
